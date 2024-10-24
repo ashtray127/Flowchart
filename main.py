a8c95f6fc4ca702eadecc45470102126ce483225
sch <github@doxle.net>
2024-10-24 08:42:26 -0400

    Remaking to clean it up

--- main.py
+++ main.py
@@ -1,205 +1,45 @@
 import pygame as pyg
 
-# ---------------- CONSTANTS
-X = 0
-Y = 1
+# ------------- CONSTS
 
-class Shapes:
-  SQUARE = "s"
-  RHOMBUS = "r"
-  PARRALELLOGRAM = "p"
-  CIRCLE = "c"
-  OVAL = "o"
+# ------------- VARS
 
-class Defaults:
-  DIAMETER = 100
-  POSITION = (640, 360)
-  COLOR = "black"
-  TEXT = ""
-  
-  def rectPoints(pos):
-    Xoffset = 60
-    Yoffset = 35
-    
-    top_left = (pos[X]-Xoffset, pos[Y]+Yoffset)
-    top_right = (pos[X]+Xoffset, pos[Y]+Yoffset)
-    
-    bottom_right = (pos[X]+Xoffset, pos[Y]-Yoffset)
-    bottom_left = (pos[X]-Xoffset, pos[Y]-Yoffset)
-    
-    return (top_left, top_right, bottom_right, bottom_left)
-  
-  def rhombusPoints(pos):
-    offset = 50
-    
-    top = (pos[X], pos[Y]+offset)
-    left = (pos[X]-offset, pos[Y])
-    bottom = (pos[X], pos[Y]-offset)
-    right = (pos[X]+offset, pos[Y])
-    
-    return (top, left, bottom, right)
+# ------------- CLASSES
+class Object:
+    def __init__(self):
+        pass
 
-  def ovalPoints(pos):
-    Xoffset = 60
-    Yoffset = 40
+    def getPos(self):
+        pass
 
-    top_left = (pos[X]-Xoffset, pos[Y]+Yoffset)
-    top_right = (pos[X]+Xoffset, pos[Y]+Yoffset)
-    
-    bottom_right = (pos[X]+Xoffset, pos[Y]-Yoffset)
-    bottom_left = (pos[X]-Xoffset, pos[Y]-Yoffset)
-    
-    return (top_left, top_right, bottom_right, bottom_left)
+    def drawObj(self):
+        pass
 
-    
+    def addConnection(self):
+        pass
 
-# ----------------- PYGAME VARS
+    def getArea(self):
+        pass
 
+# ------------ GLOBAL FUNCS
+def convert_pos(pos):
+    pass
+
+def create_id():
+    pass
+
+
+
+# ------------- PYGAME
 pyg.init()
 screen = pyg.display.set_mode((1280, 720))
 clock = pyg.time.Clock()
 dt = 0
 running = True
 
-# ----------------- OBJECT VARS
-nextId = 0
 
-class Quad:
-  def __init__(self, points):
-    self.points = points
 
-class Ellipse:
-  def __init__(self, points):
-    self.points = points
-
-class Square(Quad):
-  TOPLEFT = 0
-  TOPRIGHT = 1
-  BOTTOMRIGHT = 2
-  BOTTOMLEFT = 3
-  
-class Rectangle(Quad):
-  TOPLEFT = 0
-  TOPRIGHT = 1
-  BOTTOMRIGHT = 2
-  BOTTOMLEFT = 3
-  
-
-class Rhombus(Quad):
-  TOP = 0
-  LEFT = 1
-  BOTTOM = 2
-  RIGHT = 3
-    
-
-class Oval(Ellipse):
-  x_dia = 0
-  y_dia = 1
-  
-class Circle(Ellipse):
-  pass;
-    
-    
-# -----------------------------
-
-# ------------- GLOBAL FUNCTIONS
-
-def get_next_id():
-    global nextId
-    nextId+=1
-    return nextId
-
-def get_arrow_lines(startObj, endObj):
-  pass 
-
-def check_if_pos_empty(pos):
-    pass
-
-def draw_obj(obj):
-  
-  match obj.shape:
-    case Rhombus() | Rectangle():
-      pyg.draw.polygon(screen, obj.color, obj.shape.points)
-    
-    case Oval():
-      width = obj.shape.points[1][X] - obj.shape.points[0][X]
-      height = obj.shape.points[0][Y] - obj.shape.points[3][Y]
-
-      pyg.draw.ellipse(screen, obj.color, pyg.Rect(*obj.shape.points[3], width, height))
-    
-
-# ----------------------------
-
-objects = []
-
-class Object:
-    def __init__(self, *args, **kwargs):
-      # Get Pos
-        try:
-            self.pos = kwargs["pos"]
-        except KeyError:
-            self.pos = Defaults.POSITION
-
-        # Get Text
-        try:
-            self.text = kwargs["text"]
-        except KeyError:
-            self.text = Defaults.TEXT
-
-        # Get Color
-        try: 
-          self.color = kwargs["color"]
-        except KeyError:
-          self.color = Defaults.COLOR
-        
-          
-        self.id = get_next_id()
-        
-        self.connections = {}
-        
-        
-        
-    def getPos(self):
-      return pyg.Vector2(*self.pos)
-    
-    def addNewConnection(self, id):
-      objToConnect = objects[id]
-      self.connections[id] = { } # TODO: Make this have a point on the base object AND the object it's connected to
-      
-class Action(Object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        try:
-          self.shape = kwargs["shape"]
-        except KeyError:
-          self.shape = Rectangle(Defaults.rectPoints(self.pos))
-
-class Condition(Object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        try: 
-          self.shape = kwargs["shape"]
-        except KeyError:
-          self.shape = Rhombus(Defaults.rhombusPoints(self.pos))
-
-class StartStop(Object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        try:
-            self.shape = kwargs["shape"]
-        except KeyError:
-            self.shape = Oval(Defaults.ovalPoints(self.pos))
-
-objects.extend([
-    Condition(pos=(500, 500)),
-    Action(pos=(400,400)),
-    StartStop(),
-])
-
-# -------------------------------- GAME LOOP
+# -------------- LOOP
 while running:
 
     for event in pyg.event.get():
@@ -209,13 +49,10 @@ while running:
     screen.fill('white')
     #--------------------- DISPLAY
 
-    for obj in objects:
-        draw_obj(obj)
+
 
 
     #---------------------------
     pyg.display.flip()
     dt = clock.tick(60) / 1000
 pyg.quit()
-
-
---
+++ old.py
@@ -0,0 +1,224 @@
+import pygame as pyg
+
+# ---------------- CONSTANTS
+X = 0
+Y = 1
+
+class Shapes:
+  SQUARE = "s"
+  RHOMBUS = "r"
+  PARRALELLOGRAM = "p"
+  CIRCLE = "c"
+  OVAL = "o"
+
+class Defaults:
+  DIAMETER = 100
+  POSITION = (32, 18)
+  COLOR = "black"
+  TEXT = ""
+  
+  def rect_points(pos: list|tuple):
+    x_offset = 60
+    y_offset = 35
+    
+    top_left = (pos[X]-x_offset, pos[Y]+y_offset)
+    top_right = (pos[X]+x_offset, pos[Y]+y_offset)
+    
+    bottom_right = (pos[X]+y_offset, pos[Y]-y_offset)
+    bottom_left = (pos[X]-y_offset, pos[Y]-y_offset)
+    
+    return (top_left, top_right, bottom_right, bottom_left)
+  
+  def rhombus_points(pos:list|tuple):
+    offset = 50
+    
+    top = (pos[X], pos[Y]+offset)
+    left = (pos[X]-offset, pos[Y])
+    bottom = (pos[X], pos[Y]-offset)
+    right = (pos[X]+offset, pos[Y])
+    
+    return (top, left, bottom, right)
+
+  def oval_points(pos:list|tuple):
+    x_offset = 60
+    y_offset = 40
+
+    top_left = (pos[X]-x_offset, pos[Y]+y_offset)
+    top_right = (pos[X]+x_offset, pos[Y]+y_offset)
+    
+    bottom_right = (pos[X]+x_offset, pos[Y]-y_offset)
+    bottom_left = (pos[X]-x_offset, pos[Y]-y_offset)
+    
+    return (top_left, top_right, bottom_right, bottom_left)
+
+    
+
+# ----------------- PYGAME VARS
+
+pyg.init()
+screen = pyg.display.set_mode((1280, 720))
+clock = pyg.time.Clock()
+dt = 0
+running = True
+
+# ----------------- OBJECT VARS
+nextId = 0
+
+class Quad:
+  def __init__(self, points):
+    self.points = points
+
+class Ellipse:
+  def __init__(self, points):
+    self.points = points
+
+class Square(Quad):
+  TOPLEFT = 0
+  TOPRIGHT = 1
+  BOTTOMRIGHT = 2
+  BOTTOMLEFT = 3
+  
+class Rectangle(Quad):
+  TOPLEFT = 0
+  TOPRIGHT = 1
+  BOTTOMRIGHT = 2
+  BOTTOMLEFT = 3
+  
+
+class Rhombus(Quad):
+  TOP = 0
+  LEFT = 1
+  BOTTOM = 2
+  RIGHT = 3
+    
+
+class Oval(Ellipse):
+  x_dia = 0
+  y_dia = 1
+  
+class Circle(Ellipse):
+  pass;
+    
+    
+# -----------------------------
+
+# ------------- GLOBAL FUNCTIONS
+
+def convert(point):
+    return point*20
+
+def get_next_id():
+    global nextId
+    nextId+=1
+    return nextId
+
+def get_arrow_lines(startObj, endObj):
+  pass 
+
+def check_if_pos_empty(pos):
+    pass
+
+def draw_obj(obj):
+  
+  match obj.shape:
+    case Rhombus() | Rectangle():
+      pyg.draw.polygon(screen, obj.color, obj.shape.points)
+    
+    case Oval():
+      width = obj.shape.points[1][X] - obj.shape.points[0][X]
+      height = obj.shape.points[0][Y] - obj.shape.points[3][Y]
+
+      pyg.draw.ellipse(screen, obj.color, pyg.Rect(*obj.shape.points[3], width, height))
+    
+
+# ----------------------------
+
+objects = []
+
+class Object:
+    def __init__(self, *args, **kwargs):
+      # Get Pos
+        try:
+            self.pos = kwargs["pos"]
+        except KeyError:
+            self.pos = Defaults.POSITION
+
+        # Get Text
+        try:
+            self.text = kwargs["text"]
+        except KeyError:
+            self.text = Defaults.TEXT
+
+        # Get Color
+        try: 
+          self.color = kwargs["color"]
+        except KeyError:
+          self.color = Defaults.COLOR
+        
+          
+        self.id = get_next_id()
+        
+        self.connections = {}
+        
+        
+        
+    def getPos(self):
+      return pyg.Vector2(*self.pos)
+    
+    def addNewConnection(self, id):
+      objToConnect = objects[id]
+      self.connections[id] = { } # TODO: Make this have a point on the base object AND the object it's connected to
+      
+class Action(Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        try:
+          self.shape = kwargs["shape"]
+        except KeyError:
+          self.shape = Rectangle(Defaults.rect_points(self.pos))
+
+class Condition(Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        try: 
+          self.shape = kwargs["shape"]
+        except KeyError:
+          self.shape = Rhombus(Defaults.rhombus_points(self.pos))
+
+class StartStop(Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        try:
+            self.shape = kwargs["shape"]
+        except KeyError:
+            self.shape = Oval(Defaults.oval_points(self.pos))
+
+objects.extend([
+    Condition(pos=(500, 500)),
+    Action(pos=(400,400)),
+    StartStop(),
+])
+
+# -------------------------------- GAME LOOP
+while running:
+
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            running = False
+
+    screen.fill('white')
+    #--------------------- DISPLAY
+
+    for obj in objects:
+        draw_obj(obj)
+
+
+    #---------------------------
+    pyg.display.flip()
+    dt = clock.tick(60) / 1000
+pyg.quit()
+
+
